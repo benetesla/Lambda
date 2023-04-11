98dc72e7d1b72d16e7377573af8fc5ac245e2bc6
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-11 16:47:30 -0300

    Criando um sistema em java

--- src/App.java
+++ src/App.java
@@ -3,16 +3,12 @@ import java.util.Arrays;
 import br.com.lambda.Usuario;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        
+    public static void main(String[] args) {
+
         Usuario user1 = new Usuario("Benevanio", 150);
         Usuario user2 = new Usuario("BeneTesla", 120);
         Usuario user3 = new Usuario("Beniel", 190);
-
         List<Usuario> usuarios = Arrays.asList(user1, user2, user3);
-
-        for (Usuario u : usuarios) {
-            System.out.println(u.getNome() + " - " + u.getPontos());
-        }
+        usuarios.forEach(u -> System.out.println(u.getNome() + " - " + u.getPontos()));
     }
 }
