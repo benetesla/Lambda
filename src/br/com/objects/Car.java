a1445cb05f32f7d5113a08f62aa30cd9146f6a81
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-15 10:26:01 -0300

    Criando o sistema

---
+++ src/br/com/objects/Car.java
@@ -0,0 +1,47 @@
+package br.com.objects;
+
+public class Car {
+    private String model;
+    private String color;
+    private int year;
+    private double price;
+
+    public Car(String model, String color, int year, double price) {
+        this.model = model;
+        this.color = color;
+        this.year = year;
+        this.price = price;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public String getColor() {
+        return this.color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public double getPrice() {
+        return this.price;
+    }
+    public void setPrice(double price) {
+        this.price = price;
+    }    
+
+}
---
+++ src/br/com/objects/Object.java
@@ -0,0 +1,8 @@
+package br.com.objects;
+
+public class Object {
+    public static void main(String[] args) {
+        Car car = new Car("Ferrari", "Red", 2019, 1000000);
+        System.out.println(car.getModel()+ " " + car.getColor() + " " + car.getYear() + " " + car.getPrice());
+    }
+}
