1fe9e916ff4ddc9a2df53d45556b758d9d474194
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-15 10:38:36 -0300

    Subindo a nossa classe objeto para o github

---
+++ src/br/com/objects/Human.java
@@ -0,0 +1,54 @@
+package br.com.objects;
+
+public class Human {
+    private String name;
+    private int age;
+    private double height;
+    private double weight;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return this.age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public double getHeight() {
+        return this.height;
+    }
+
+    public void setHeight(double height) {
+        this.height = height;
+    }
+
+    public double getWeight() {
+        return this.weight;
+    }
+
+    public void setWeight(double weight) {
+        this.weight = weight;
+    }
+
+    public Human(String name, int age, double height, double weight) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+        this.weight = weight;
+    }
+
+    public void eat() {
+        System.out.println("Eating...");
+    }
+    public void sleep() {
+        System.out.println("Sleeping...");
+    }
+}
--- src/br/com/objects/Object.java
+++ src/br/com/objects/Object.java
@@ -5,5 +5,10 @@ public class Object {
         Car car = new Car("Ferrari", "Red", 2019, 1000000);
         System.out.println(car.getModel()+ " " + car.getColor() + " " + car.getYear() + " " + car.getPrice());
         car.drive();
+
+        Human human = new Human("John", 25, 1.75, 70);
+        System.out.println(human.getName() + " " + human.getAge() + " " + human.getHeight() + " " + human.getWeight());
+        human.eat();
+        human.sleep();
     }
 }
