849481efeda2126933e6fc2a1d7c1e4cbf015b82
Bene_Tesla <78994881+benetesla@users.noreply.github.com>
2023-04-16 10:23:37 -0300

    Criando o sistema usando o string methods

---
+++ src/br/com/stringmethods/Food.java
@@ -0,0 +1,17 @@
+package br.com.stringmethods;
+
+public class Food {
+    private String name;
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    Food(String name) {
+        this.name = name;
+    }
+}
---
+++ src/br/com/stringmethods/StrinM.java
@@ -0,0 +1,17 @@
+package br.com.stringmethods;
+
+public class StrinM {
+    public static void main(String[] args) {
+    
+        Food [] foods = new Food[3];
+        Food food1 = new Food("Pizza");
+        Food food2 = new Food("Hamburguer");
+        Food food3 = new Food("Hot Dog");
+        foods[0] = food1;
+        foods[1] = food2;
+        foods[2] = food3;
+        for (Food food : foods) {
+            System.out.println(food.getName());
+        }
+    }
+}
